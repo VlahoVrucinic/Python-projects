c8c759fcabae5639fb5c22a84978c37797734a5b
VlahoVrucinic <91995878+VlahoVrucinic@users.noreply.github.com>
2022-08-03 14:37:10 +0200

    Update class_example.py

--- class_example.py
+++ class_example.py
@@ -1,21 +1,18 @@
-	class Profil:
-
-    profil_counter = 0
-
+class Profile:
+    
     def __init__(self):
         self.nickname = input('Enter a nickname: ')
         self.password = input('Enter your password: ')
         self.email = input('Enter your e-mail: ')
     
     
-    def display_profil(self):
+    def display_profile(self):
         print('Nickname: ' + self.nickname + '\npassword: ' + self.password + '\nE-mail: ' + self.email)
     
 
-profil1 = Profil()
-profil2 = Profil()
-profil1.display_profil()
-
+profil1 = Profile()
+print('\n')
+profil1.display_profile()
 
 
 
